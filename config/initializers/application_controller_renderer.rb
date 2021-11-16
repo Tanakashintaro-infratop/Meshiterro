1c37295e09d5f57d1096889810db9bbed3abf2d5
Tanaka.Shintaro.infratop <tanaka.shintaro.infratop@gmail.com>
2021-11-16 05:41:37 +0000

    first commit

---
+++ config/initializers/application_controller_renderer.rb
@@ -0,0 +1,9 @@
+# Be sure to restart your server when you modify this file.
+
+# ActiveSupport::Reloader.to_prepare do
+#   ApplicationController.renderer.defaults.merge!(
+#     http_host: 'example.org',
+#     https: false
+#   )
+# end
+Refile.secret_key = '73aed6969a7131b13876c2072054f5e03113503e4510f74f107aa8f5092bc31679f1abed523531338dca5f01d7f2cf9c837bf16d3676dfbab7bc4a0f5ccf4d19'
